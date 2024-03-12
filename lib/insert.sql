f0cdec03b7568d025ef2a7d4ba35e7a25bf2681d
[james-mbuvi] <[james.mbuvi@student.moringaschool.com]>
2024-03-12 09:08:16 +0300

    compulsory lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+  ('Mr. Chocolate', 5, 'M', 'brown', 'friendly', 1),
+  ('Rowdy', 3, 'M', 'black', 'playful', 1),
+  ('Tabitha', 4, 'F', 'gray', 'calm', 1),
+  ('Sergeant Brown', 7, 'M', 'brown', 'serious', 1),
+  ('Melissa', 6, 'F', 'brown', 'gentle', 1),
+  ('Grinch', 10, 'M', 'green', 'grumpy', 0),
+  ('Wendy', 2, 'F', 'white', 'energetic', 1),
+  (NULL, 8, 'M', 'black', 'unknown', 1);
